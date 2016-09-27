a5940a4b454098d3d690e1e7ab817d4412d91677
Snow <Snow@Snow.fios-router.home>
2016-09-27 01:15:02 -0400

    change for the interest rate

--- src/Question1/Q1.java
+++ src/Question1/Q1.java
@@ -6,38 +6,20 @@ public class Q1 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-
-
 			
-		System.out.print("Enter tuition interest rate for the second year: ");
+		System.out.print("Enter tuition interest rate: ");
 			
 			double interestrate1 = input.nextDouble();
-		
-		System.out.print("Enter tuition interest rate for the third year: ");
-			
-			double interestrate2 = input.nextDouble();
-			
-		System.out.print("Enter tuition interest rate for the fourth year: ");
-			
-			double interestrate3 = input.nextDouble();
-				
-			double tuition = 10000;
-			
-			
-			double convertrate1 = (interestrate1/100)+1; 
-			
-			double convertrate2 = (interestrate2/100)+1; 
-			
-			double convertrate3 = (interestrate3/100)+1; 
-			
-			
-			
 					
+			double tuition = 12342;
+					
+			double convertrate1 = (interestrate1/100)+1; 
+												
 			double tuition2 = tuition * convertrate1;
 					
-			double tuition3 = tuition2 * convertrate2;
+			double tuition3 = tuition2 * convertrate1;
 					
-			double tuition4 = tuition3 * convertrate3;
+			double tuition4 = tuition3 * convertrate1;
 					
 			double tuitiontotal = tuition + tuition2 + tuition3 + tuition4;
 
